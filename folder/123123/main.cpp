40c66bc09ccb3d1e220b2cfaae7e69462fcf5b2f
michalptak <26803765+michalptak@users.noreply.github.com>
2021-07-01 17:46:51 +0200

    Add Fibonacci algorithm

--- folder/123123/main.cpp
+++ folder/123123/main.cpp
@@ -1,9 +1,26 @@
-#include <iostream>
-
+#include<iostream>
 using namespace std;
+
+void fibonacci(int n)
+{    
+     long long a = 0, b = 1;
+  
+     for(int i=0;i<n;i++)
+     {
+            cout<<b<<" ";
+            b += a;
+            a = b-a;
+     }     
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int n;
+    
+    cout<<"Podaj ile chcesz wypisać wyrazów ciągu fibonacciego: ";
+    cin>>n;
+    
+    fibonacci(n);
+    
     return 0;
 }
