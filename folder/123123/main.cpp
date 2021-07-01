6efe5aac2b9fcf37d7aa3668dfa9d117cb15d127
kroken1 <olefilip9@gmail.com>
2021-07-01 18:26:03 +0200

    merge bug1 to main

--- folder/123123/main.cpp
+++ folder/123123/main.cpp
@@ -2,26 +2,25 @@
 using namespace std;
 
 void fibonacci(int n)
-{    
+{
      long long a = 0, b = 1;
-  
+
      for(int i=0;i<n;i++)
      {
             cout<<b<<" ";
             b += a;
             a = b-a;
-     }     
+     }
 }
 
 int main()
 {
 
-    int n=5;
-    
+    const int n=5;
+
 	cout<<"bedzie konflikt";
 
-
     fibonacci(n);
-    
+
     return 0;
 }
